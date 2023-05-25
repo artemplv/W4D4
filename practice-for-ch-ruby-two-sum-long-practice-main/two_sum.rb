e2f39f5b09afd8ea6856f93d95a2b80cb5f821b5
artemplv <artemplv@gmail.com>
2023-05-25 14:49:57 -0400

    solve two_sum with sorting

--- practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
+++ practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
@@ -7,6 +7,34 @@ def two_sum?(arr, target)
   false
 end
 
+# arr = [0, 1, 5, 7]
+# p two_sum?(arr, 6) # => should be true
+# p two_sum?(arr, 10) # => should be false
+
+
+def two_sum_2?(arr, target)
+  arr.sort
+  left = 0
+  right = arr.length - 1
+
+  until left == right
+    second_target = arr[left] + arr[right]
+    
+    if second_target == target
+      return true
+    end
+
+    if second_target < target
+      left += 1
+    else
+      right -= 1
+    end
+  end
+
+  return false
+end
+
 arr = [0, 1, 5, 7]
-p two_sum?(arr, 6) # => should be true
-p two_sum?(arr, 10) # => should be false
+p two_sum_2?(arr, 6) # => should be true
+p two_sum_2?(arr, 10) # => should be false
+
